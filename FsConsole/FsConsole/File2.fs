1c75d67acd8500fc3a2504c47e4762787b526bc5
Niklas Dahlman <nd@sql8r.net>
2016-04-03 21:17:34 +0200

    f# stuff

---
+++ FsConsole/FsConsole/File2.fs
@@ -0,0 +1,11 @@
+﻿namespace FsConsole
+
+    module File2 =
+        open System
+    
+        let names x = 
+            match x with
+            | 2.71 -> "e"
+            | 3.14 -> "pi"
+            | _ -> "unknown"
+
---
+++ FsConsole/FsConsole/Program.fs
@@ -0,0 +1,16 @@
+﻿
+namespace FsConsole
+    open System
+
+    module Program =
+
+        [<EntryPoint>]
+        let Main  args = 
+            
+            File1.runMyTests
+
+            File3.showFamily
+            
+            Console.ReadKey true |> ignore
+            0
+  

549477e35b8fce8101ef8f3b92e4bdc825987d7f
Niklas Dahlman <nd@sql8r.net>
2017-08-19 15:25:18 +0200

    send grid

---
+++ MailSending/MailSendingService/Services/MailSendingTopService.cs
@@ -0,0 +1,27 @@
+﻿using MailSendingService.Interfaces;
+
+namespace MailSendingService.Services
+{
+    public class MailSendingTopService : IMailSendingTopService
+	{
+        public MailSendingTopService(IMailSendingService svc)
+        {
+            _mailerService = svc;
+        }
+
+        private readonly IMailSendingService _mailerService;
+
+        public bool Start()
+        {
+	        bool ok = _mailerService.TestMethod();
+
+            return true;
+        }
+
+        public bool Stop()
+        {
+
+            return true;
+        }
+    }
+}
